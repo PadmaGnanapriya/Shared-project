54b3667b1b48b9274756d1b3f42702b020e4c1dc
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-06 05:24:46 +0530

    Delete xml and create new xml for dash board due to errors

--- src/lk/uok/AppInitializer.java
+++ src/lk/uok/AppInitializer.java
@@ -6,18 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+//
+//public class AppInitializer extends Application {
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("view/DashBoardForm.fxml"));
+//        primaryStage.setTitle("Thoga Kade");
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
+
 public class AppInitializer extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/DashBoardForm.fxml"));
-        primaryStage.setTitle("Thoga Kade");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.
+                load(this.getClass().
+                        getResource("view/DashBoardForm.fxml"))));
+        primaryStage.show();
     }
 }
