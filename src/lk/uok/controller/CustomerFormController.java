5d0e4b98400468463b29fcbe3a1bdacb3c125ba4
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-06 18:13:31 +0530

    Detect Selected row data into textFields

--- src/lk/uok/controller/CustomerFormController.java
+++ src/lk/uok/controller/CustomerFormController.java
@@ -1,13 +1,17 @@
 package lk.uok.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import lk.uok.dao.DatabaseAccessCode;
 import lk.uok.dto.CustomerDTO;
 import lk.uok.view.tm.CustomerTM;
@@ -45,6 +49,7 @@ public class CustomerFormController extends Component {
         colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
         colOperation.setCellValueFactory(new PropertyValueFactory<>("btn"));
         loadAllCustomers();
+
     }
 
 
@@ -86,4 +91,20 @@ public class CustomerFormController extends Component {
         CustomerDTO dto=new CustomerDTO(txtId.getText(),txtName.getText(),txtAddress.getText(),Double.parseDouble(txtSalary.getText()));
         DatabaseAccessCode.updateCustomer(dto);
     }
+
+    public void tableClickAction(SortEvent<TableView> tableViewSortEvent) {
+        System.out.println("POOIOO");
+    }
+
+    public void onMouseClick(MouseEvent mouseEvent) {
+        ObservableList<CustomerTM> selectedRows = tbl.getSelectionModel().getSelectedItems();
+        txtId.setText(selectedRows.get(0).getId());
+        txtName.setText(selectedRows.get(0).getName());
+        txtAddress.setText(selectedRows.get(0).getAddress());
+        txtSalary.setText(String.valueOf(selectedRows.get(0).getSalary()));
+    }
+
+
+//    CustomerTM customerTM= (CustomerTM) tbl.getSelectionModel().getSelectedItems();
+
 }
