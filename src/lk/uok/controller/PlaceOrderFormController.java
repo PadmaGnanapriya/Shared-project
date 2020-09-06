9ac85f43e492efcc42ec53ccdeb861cd66d6cdfe
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-06 23:01:35 +0530

    Orderform Table's activities were created

--- src/lk/uok/controller/PlaceOrderFormController.java
+++ src/lk/uok/controller/PlaceOrderFormController.java
@@ -3,10 +3,10 @@ package lk.uok.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import lk.uok.dao.DatabaseAccessCode;
 import lk.uok.dao.DatabaseAccessItem;
@@ -14,7 +14,10 @@ import lk.uok.dto.CustomerDTO;
 import lk.uok.dto.ItemDTO;
 import lk.uok.view.tm.CustomerTM;
 import lk.uok.view.tm.ItemTM;
+import lk.uok.view.tm.PlaceOrderTM;
 
+import javax.swing.*;
+import java.awt.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -39,8 +42,16 @@ public class PlaceOrderFormController {
     public ComboBox cmbXustomerID;
     public TextField txtCustomerName;
     public TextField txtTotal;
+    public Button btnAdd;
+
+    ObservableList<PlaceOrderTM> orderTMS=FXCollections.observableArrayList();
 
     public void initialize() throws SQLException, ClassNotFoundException {
+        colCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        colQtyOnHand.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
         loadCustomer();
         loadItem();
     }
@@ -52,7 +63,6 @@ public class PlaceOrderFormController {
             itemCodes.add(dto.getCode());
         }
         cmbItemCode.setItems(itemCodes);
-
     }
 
     private void loadCustomer() throws SQLException, ClassNotFoundException {
@@ -65,21 +75,62 @@ public class PlaceOrderFormController {
     }
 
     public void addOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        if(txtCustomerName.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Please Select Customer Id", "alert", JOptionPane.ERROR_MESSAGE);
+            cmbXustomerID.requestFocus();
+            return;
+        }
+        if(txtQty.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Please Select amount of item", "alert", JOptionPane.ERROR_MESSAGE);
+            txtQty.requestFocus();
+            return;
+        }
         ItemDTO itemDTO=DatabaseAccessItem.searchItem(cmbItemCode.getValue().toString());
-//        ObservableList<>
-//        tbl.setItems();
+
+        int priviousQty = 0;
+        double tot=0;
+        for(int i=0; i<orderTMS.size();i++){
+            if(cmbItemCode.getValue().toString().equals(orderTMS.get(i).getCode())){
+                priviousQty=orderTMS.get(i).getQty();
+                orderTMS.remove(i);
+            }
+        }
+        PlaceOrderTM tm=new PlaceOrderTM(itemDTO.getCode(),itemDTO.getDescription(),itemDTO.getUnitPrice(),Integer.parseInt(txtQty.getText())+priviousQty);
+        orderTMS.add(tm);
+        tbl.setItems(orderTMS);
+        System.out.println(orderTMS.size());
+        for(int i=0;i<orderTMS.size();i++) tot+=orderTMS.get(i).getTotal();
+        txtTotal.setText(String.valueOf(tot));
+        cmbItemCode.requestFocus();
     }
 
     public void cancleOnAction(ActionEvent actionEvent) {
+        txtDescription.setText("");
+        txtUnitPrice.setText("");
+        txtQtyONHand.setText("");
+        txtQty.setText("");
+        cmbItemCode.requestFocus();
     }
 
     public void onMouseClick(MouseEvent mouseEvent) {
     }
 
-    public void cancleOrderOnAction(ActionEvent actionEvent) {
+    public void cancleOrderOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        orderTMS.clear();
+        tbl.setItems(orderTMS);
+        txtTotal.setText("");
+        txtCustomerName.setText("");
+//       cmbItemCode.setItems(null);
+//       loadCustomer();
+//       loadItem();
+        cancleOnAction(actionEvent);
+
     }
 
-    public void placeOderOnAction(ActionEvent actionEvent) {
+    public void placeOderOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+//        for(PlaceOrderTM tm:PlaceOrderTM)
+//        ItemDTO itemDTO =new ItemDTO()
+//        DatabaseAccessItem.updateItem(itemDTO);
     }
 
     public void customerIdOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
@@ -92,6 +143,12 @@ public class PlaceOrderFormController {
         txtDescription.setText(itemDTO.getDescription());
         txtUnitPrice.setText(String.valueOf(itemDTO.getUnitPrice()));
         txtQtyONHand.setText(String.valueOf(itemDTO.getQtyOnHand()));
+        txtQty.setText("");
+        txtQty.requestFocus();
 
+    }
+
+    public void qtyOnAction(ActionEvent actionEvent) {
+        btnAdd.requestFocus();
     }
 }
--- src/lk/uok/view/tm/PlaceOrderTM.java
+++ src/lk/uok/view/tm/PlaceOrderTM.java
@@ -6,4 +6,60 @@ package lk.uok.view.tm;
 
 
 public class PlaceOrderTM {
+    private String code;
+    private String description;
+    private double unitPrice;
+    private int qty;
+    private double total;
+
+    public PlaceOrderTM(String code, String description, double unitPrice, int qty) {
+        this.code = code;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.qty = qty;
+        this.total = qty * unitPrice;
+    }
+
+    public PlaceOrderTM() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
 }
